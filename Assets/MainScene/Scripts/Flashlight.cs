d609041935e5728dd65daaa88917b3bd3a30a4e3
Jamie Wright <wrightjamie360@icloud.com>
2024-10-28 18:11:27 +0000

    Flashlight Testing

--- Assets/MainScene/Scripts/Flashlight.cs
+++ Assets/MainScene/Scripts/Flashlight.cs
@@ -9,16 +9,18 @@ public class Flashlight : MonoBehaviour
     public bool isOn = false;
     public GameObject lightSource;
     public AudioSource clickSound;
+    public AudioSource IntenseSound;
     public bool failSafe = false;
 
     void Update()
     {
-        if (Input.GetButtonDown("F"))
+        if (Input.GetButtonDown("LightOnOff"))
         {
             if (isOn == false && failSafe == false)
             {
                 failSafe = true;
                 lightSource.SetActive(true);
+                clickSound.Play();
                 isOn = true;
                 StartCoroutine(FailSafe());
             }
@@ -26,15 +28,24 @@ public class Flashlight : MonoBehaviour
             {
                 failSafe = true;
                 lightSource.SetActive(false);
+                clickSound.Play();
                 isOn = false;
                 StartCoroutine(FailSafe());
+            }
+        }
+        if (Input.GetButtonDown("Intense"))
+        {
+            IntenseSound.Play();
+            if (lightSource == true)
+            {
+                
             }
         }
     }
 
     IEnumerator FailSafe()
     {
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(0.1f);
         failSafe = false;
     }
 }
