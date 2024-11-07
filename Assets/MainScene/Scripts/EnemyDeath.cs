b8d9a80bf505765e5f957d132379eb9080b6a9c5
Jamie Wright <wrightjamie360@icloud.com>
2024-11-07 16:03:30 +0000

    EnemyScripting

---
+++ Assets/MainScene/Scripts/EnemyDeath.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public int EnemyHealth = 100;
+    public GameObject PlayerCollider;
+
+}
