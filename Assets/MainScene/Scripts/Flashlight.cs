300ff15ba3753fd5ef7f695df31b58ca7bfd7cbe
Jamie Wright <wrightjamie360@icloud.com>
2024-11-12 15:40:13 +0000

    +1

--- Assets/MainScene/Scripts/Flashlight.cs
+++ Assets/MainScene/Scripts/Flashlight.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
