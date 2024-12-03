dc077bbd9b42e218b73eaceeb03025525bef39bd
Jamie Wright <wrightjamie360@icloud.com>
2024-12-03 16:04:03 +0000

    Add ons

--- Assets/MainScene/Scripts/CollisionScript.cs
+++ Assets/MainScene/Scripts/CollisionScript.cs
@@ -13,6 +13,31 @@ public class CollisionScript : MonoBehaviour
     public bool light1 = false;    
     public bool light2 = false;
 
+    public AudioSource growlSound;
+
+    public SkinnedMeshRenderer skinnedMesh;
+    public float dissolveRate = 0.0125f;
+    public float refreshRate = 0.025f;
+    public int mainLight;
+
+    private Material[] skinnedMaterials;
+
+    void Start()
+    {
+        if (skinnedMesh != null)
+            skinnedMaterials = skinnedMesh.materials;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(DissolveCo());
+        }
+    }
+
+
+
     void OnTriggerStay(UnityEngine.Collider other)
     {
         if (Input.GetKey(KeyCode.Space))
@@ -27,6 +52,24 @@ public class CollisionScript : MonoBehaviour
             //Destroy(other.gameObject);
             light1 = false;
             light2 = false;
+        }
+    }
+
+    IEnumerator DissolveCo()
+    {
+        if (skinnedMaterials.Length > 0)
+        {
+            growlSound.Play();
+            float counter = 0;
+            while (skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+            {
+                counter += dissolveRate;
+                for (int i = 0; i < skinnedMaterials.Length; i++)
+                {
+                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+                }
+                yield return new WaitForSeconds(refreshRate);
+            }
         }
     }
 }
--- Assets/MainScene/Scripts/DissolvingController.cs
+++ Assets/MainScene/Scripts/DissolvingController.cs
@@ -3,45 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal.Internal;
 
-public class DissolvingController : MonoBehaviour
-{
-    public SkinnedMeshRenderer skinnedMesh;
-    public float dissolveRate = 0.0125f;
-    public float refreshRate = 0.025f;
-    public int mainLight;
+//public class DissolvingController : MonoBehaviour
+//{
+//    public SkinnedMeshRenderer skinnedMesh;
+//    public float dissolveRate = 0.0125f;
+//    public float refreshRate = 0.025f;
+//    public int mainLight;
 
-    private Material[] skinnedMaterials; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (skinnedMesh != null)
-            skinnedMaterials = skinnedMesh.materials;
-    }
+//    private Material[] skinnedMaterials; 
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        if (skinnedMesh != null)
+//            skinnedMaterials = skinnedMesh.materials;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            StartCoroutine(DissolveCo());
-        }
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if(Input.GetKeyDown(KeyCode.Space))
+//        {
+//            StartCoroutine(DissolveCo());
+//        }
+//    }
 
-    IEnumerator DissolveCo()
-    {
-        if(skinnedMaterials.Length > 0)
-        {
-            float counter = 0;  
+//    IEnumerator DissolveCo()
+//    {
+//        if(skinnedMaterials.Length > 0)
+//        {
+//            float counter = 0;  
 
-            while(skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
-            {
-                counter += dissolveRate;
-                for(int i=0; i<skinnedMaterials.Length; i++)
-                {
-                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
-                }
-                yield return new WaitForSeconds(refreshRate);  
-            }
-        }
-    }
-}
+//            while(skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
+//            {
+//                counter += dissolveRate;
+//                for(int i=0; i<skinnedMaterials.Length; i++)
+//                {
+//                    skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
+//                }
+//                yield return new WaitForSeconds(refreshRate); 
+//            }
+//        }
+//    }
+//}
